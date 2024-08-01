3fe21280055169815382f8afbfc353b0db4007c8
Storm-Whale <emphong481@gmail.com>
2024-08-01 17:51:49 +0700

    first commit

---
+++ src/main/java/com/whale/duongkieuanh/DuongkieuanhApplication.java
@@ -0,0 +1,13 @@
+package com.whale.duongkieuanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuongkieuanhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuongkieuanhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/whale/duongkieuanh/DuongkieuanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whale.duongkieuanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuongkieuanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
